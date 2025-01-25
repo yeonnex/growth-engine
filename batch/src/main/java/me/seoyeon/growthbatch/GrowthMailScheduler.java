5a16fba787e1d31e6d5ae08b9fc9e869443a1b44
yeonnex <yeonnex@gmail.com>
2025-01-25 09:34:47 +0900

    feat: 테스트 Dockerfile, docker-compose.yml, master.yml 작성

--- batch/src/main/java/me/seoyeon/growthbatch/GrowthMailScheduler.java
+++ batch/src/main/java/me/seoyeon/growthbatch/GrowthMailScheduler.java
@@ -5,11 +5,15 @@ import me.seoyeon.githubclient.dto.response.GitHubContentItem;
 import me.seoyeon.growthbatch.service.GrowthMemoItem;
 import me.seoyeon.growthbatch.service.GrowthRepoService;
 import me.seoyeon.mailclient.EmailSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GrowthMailScheduler {
+
+  private final Logger log = LoggerFactory.getLogger(GrowthMailScheduler.class);
 
   private final EmailSender eMailSender;
   private final GrowthRepoService growthRepoService;
@@ -20,8 +24,8 @@ public class GrowthMailScheduler {
   }
 
   @Scheduled(cron = "0 15 8 * * *")
-  //  @Scheduled(fixedDelay = 2000)
   public void sendMailToMe() {
+    log.info("발송 준비...");
     List<GrowthMemoItem> memoItems = growthRepoService.pickRandomGrowthMemo(3);
     List<GitHubContentItem> detailMemos = growthRepoService.getDetailMemos(memoItems);
     detailMemos.forEach(
